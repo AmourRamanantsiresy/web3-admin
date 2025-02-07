4baec65dbfb671284a87c69f3982d82fb6978040
amourRamanantsiresy <hei.amour.2@gmail.com>
2025-02-07 10:25:36 +0300

    feat: create or update configurations

--- server/src/routes/Configuration.routes.js
+++ server/src/routes/Configuration.routes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { Op } from 'sequelize';
+import { v4 } from 'uuid';
 import { Configuration } from '../models/Configuration.model.js';
 import { validateConfiguration } from '../validators/Configuration.validator.js';
 
@@ -9,12 +10,9 @@ configurationRouter.get('/', async (req, res) => {
   try {
     const { title, page = 1, perPage = 10 } = req.query;
     const offset = (page - 1) * perPage;
-    const filter = title ? { where: { title: { [Op.iLike]: `%${title}%` } } } : {};
-    const { rows: configurations, total = 0 } = await Configuration.findAll({ ...filter, limit: parseInt(perPage), offset: parseInt(offset) });
-    res.json({
-      data: configurations || [],
-      totalPage: Math.ceil(total / perPage),
-    });
+    const filter = title ? { where: { title: { [Op.like]: `%${title}%` } } } : {};
+    const configurations = await Configuration.findAll({ ...filter, limit: perPage + 1, offset: offset });
+    res.json({ data: configurations.slice(0, perPage) || [], hasNext: configurations.length > perPage });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -36,10 +34,19 @@ configurationRouter.get('/:id', async (req, res) => {
 });
 
 configurationRouter.put('/:id', validateConfiguration, async (req, res) => {
+  const { id = v4() } = req.params;
   try {
-    const config = await Configuration.create(req.body);
-    res.status(201).json(config);
+    let configurationForProvidedId = await Configuration.findByPk(id);
+
+    if (!configurationForProvidedId) {
+      configurationForProvidedId = await Configuration.create({ ...req.validatedData, id });
+    } else {
+      Object.keys(req.validatedData).forEach(key => (configurationForProvidedId[key] = req.validatedData[key]));
+      await configurationForProvidedId.save();
+    }
+    res.status(201).json(configurationForProvidedId);
   } catch (error) {
+    console.log(error);
     res.status(500).json({ error: error.message });
   }
 });
